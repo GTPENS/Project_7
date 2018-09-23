0cd6cbc55a35de79a7937b05d142649737661a95
ChusnulC <33141193+ChusnulC@users.noreply.github.com>
2018-09-23 17:14:03 +0700

    Add files via upload

---
+++ Rumble Boxing/Assets/Scripts/GameController.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public int[] quest = new int[] { 1, 2, 3, 4 };
+    int correctAnswer = 0;
+    int questNumber = 0;
+
+    public Transform[] placeHolder;
+    public GameObject[] animal;
+    public TimerScript timer;
+    public bool isPlayerAnswer = false;
+   
+    // Use this for initialization
+    void Start()
+    {
+        RandomAnimal();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void InputButton(int tipe)
+    {
+        if (tipe == 1)
+            print("A");
+        if (tipe == 2)
+            print("B");
+        if (tipe == 3)
+            print("C");
+        if (tipe == 4)
+            print("D");
+        Answer(tipe);
+    }
+
+    public void Answer(int answer)
+    {
+        if (quest[questNumber] == answer)
+        {
+            correctAnswer++;
+        }
+        else
+        {
+            print("Kamu payah");
+        }
+        if (questNumber >= 3)
+        {
+            questNumber = 0;
+
+            if (correctAnswer == 4)
+            {
+                print("Benar");
+            }
+            else
+            {
+                print("Pukul");
+            }
+            isPlayerAnswer = true;
+            correctAnswer = 0;
+            RandomizeArray();
+            RandomAnimal();
+        }
+        else
+        {
+            questNumber++;
+        }
+
+        print("Quest Number = " + questNumber);
+    }
+
+    void RandomizeArray()
+    {
+        for (int i = quest.Length - 1; i > 0; i--)
+        {
+            int r = Random.Range(0, i);
+            int tmp = quest[i];
+            quest[i] = quest[r];
+            quest[r] = tmp;
+        }
+    }
+
+    public void RandomAnimal()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                if (quest[i] == j + 1)
+                {
+                    animal[j].transform.position = placeHolder[i].position;
+                }
+            }
+        }
+    }
+
+    public void WrongEffect()
+    {
+        print("Musuh Memukul");
+        StartCoroutine(StopforAWhile());
+        
+    }
+
+    public void RightEffect()
+    {
+        print("Player Memukul");
+        StartCoroutine(StopforAWhile());
+        
+    }
+    IEnumerator StopforAWhile()
+    {
+        yield return new WaitForSeconds(1f);
+        //Time.timeScale = 1;
+        timer.TimeRunning();
+        isPlayerAnswer = false;
+    }
+
+
+}
---
+++ Rumble Boxing/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    Image timerBar;
+    public float maxTime = 5f;
+    float timeLeft;
+    public bool isTimeToStop = false;
+    public GameController gameSet;
+
+    // Use this for initialization
+    void Start()
+    {
+        timerBar = GetComponent<Image>();
+        timeLeft = maxTime;
+        StartCoroutine(CountDown());
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+            
+	}
+
+    IEnumerator CountDown()
+    {
+        float counter = maxTime;
+        while (counter > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            counter--;
+            timerBar.fillAmount = counter / maxTime;
+            if(gameSet.isPlayerAnswer == true)
+            {
+                gameSet.RightEffect();
+                //Time.timeScale = 0;
+                //timerBar.fillAmount = 1;
+            }
+            if (counter <= 0)
+            {
+                isTimeToStop = true;
+                gameSet.RandomAnimal();
+                gameSet.WrongEffect();
+                
+            }
+
+        }
+
+    }
+
+    public void TimeRunning()
+    {
+        StartCoroutine(CountDown());
+    }
+}
