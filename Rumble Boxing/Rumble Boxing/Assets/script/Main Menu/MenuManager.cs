b1a37eea9e09f3e60742bb485467d45a6c5b8afc
WINDOWS-73E4E21\RAYHAN <rahadian.rayhan@gmail.com>
2018-10-08 07:17:10 +0700

    +main menu

---
+++ Rumble Boxing/Rumble Boxing/Assets/script/Main Menu/MenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class MenuManager : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+       
+		if(gameObject.tag == "mainmenu")
+        {
+            transform.DOMoveY(3,1);
+            if (Input.GetKeyDown("space"))
+            {
+                transform.DOShakePosition(5, 15, 1);
+            }
+        }
+        if(gameObject.tag == "obj1")
+        {
+            transform.DOShakeRotation(2,10,1);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    
+}
---
+++ Rumble Boxing/Rumble Boxing/Assets/script/Main Menu/MusicManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    private AudioSource audioscr;
+    private float musicVol = 1;
+
+    // Use this for initialization
+    void Start () {
+        audioscr = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        audioscr.volume = musicVol;
+    }
+
+    public void SetVol(float vol)
+    {
+        musicVol = vol;
+    }
+}
