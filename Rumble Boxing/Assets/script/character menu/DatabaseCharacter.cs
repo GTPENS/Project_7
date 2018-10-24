1fc428cbbd4e6542669a9375dcdd85275f127e36
sepay <julian.21.akbar@gmail.com>
2018-10-24 21:52:32 +0700

    T050_New character design
    - captain america model
    - thanos model
    - database character contains name

---
+++ Rumble Boxing/Assets/script/character menu/DatabaseCharacter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatabaseCharacter : MonoBehaviour {
+
+    private static DatabaseCharacter databaseMachine = new DatabaseCharacter();
+    public DatabaseCharacter()
+    {
+        loadDatabase();
+    }
+
+    public static DatabaseCharacter getInstance()
+    {
+        return databaseMachine;
+    }
+
+    private List<ArrayList> arrayOfCharacter;
+
+    private void loadDatabase()
+    {
+        arrayOfCharacter = new List<ArrayList>()
+        {
+            new ArrayList() {"Name"},
+            new ArrayList() {"Johny"},
+            new ArrayList() {"Alex"},
+            new ArrayList() {"Captain Steve"},
+            new ArrayList() {"Thonas"}
+
+        };
+    }
+
+    public string getName(int _idCharacter)
+    {
+        return (string)arrayOfCharacter[_idCharacter][0];
+    }
+}
